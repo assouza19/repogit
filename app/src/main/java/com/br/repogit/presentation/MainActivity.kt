eada29f6b67908e7748d93cecd6147fa3ea2e99c
Aline Silva <aline.silva@picpay.com>
2021-05-26 20:36:01 -0300

    First commit - base

---
+++ app/src/main/java/com/br/repogit/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.br.repogit.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.br.repogit.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
